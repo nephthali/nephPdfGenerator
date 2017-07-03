49fbf33a5d615a9ddbb97b1554b38f07f371f921
nephty <nephthali@libero.it>
2017-07-03 10:29:15 +0200

    [NEPH-0] Fix namespace

--- src/Ennva/NephPdfBundle/CinecaNephPdfBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Cineca\NephPdfBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class CinecaNephPdfBundle extends Bundle
-{
-}
--- src/Ennva/NephPdfBundle/DependencyInjection/Configuration.php
+++ src/Ennva/NephPdfBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Cineca\NephPdfBundle\DependencyInjection;
+namespace Ennva\NephPdfBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cineca_neph_pdf');
+        $rootNode = $treeBuilder->root('Ennva_neph_pdf');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
